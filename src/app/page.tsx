d33899e02aeab6da2f9aaff8174417af872d9b68
precious654 <odukwechiemeka@gmail.com>
2024-10-29 09:17:10 +0100

    grid-handled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex justify-between">
-      <div className="pt-3 flex flex-col gap-20">
+    <main className="flex gap-10 py-5">
+      <div className="flex flex-col gap-20 justify-between w-5/12">
         <div className="flex flex-col gap-8">
           <Image
             src={profile}
@@ -65,33 +65,33 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="grid grid-cols-8 gap-x-9">
-        <div className="bg-[#F5F4F2] p-6">
+      <div className="grid grid-cols-8 gap-x-3 w-7/12 gap-y-5">
+        <div className="bg-[#F5F4F2] p-6 col-span-2 rounded-lg">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">Experience</p>
         </div>
-        <div className="bg-[#F5F4F2]">
+        <div className="bg-[#F5F4F2] p-6 col-span-4 rounded-lg">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">Projects</p>
         </div>
-        <div className="bg-[#F5F4F2]">
+        <div className="bg-[#F5F4F2] p-6 col-span-2 rounded-lg">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">Open Source</p>
         </div>
-        <div className="bg-[#F5F4F2]">
+        <div className="bg-[#F5F4F2] p-6 col-span-5">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">About</p>
         </div>
-        <div className="bg-[#F5F4F2]">
+        <div className="bg-[#F5F4F2] p-6 col-span-3">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">
             What I&apos;m listening to
           </p>
         </div>
-        <div className="bg-[#F5F4F2]">
+        <div className="bg-[#F5F4F2] p-6 col-span-2">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">
             What I&apos;m reading
           </p>
         </div>
-        <div className="bg-[#F5F4F2]">
+        <div className="bg-[#F5F4F2] p-6 col-span-2">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">Location</p>
         </div>
-        <div className="bg-[#F5F4F2]">
+        <div className="bg-[#F5F4F2] p-6 col-span-4">
           <p className="px-6 py-3 bg-[#E9E8E6] rounded-3xl">
             Featured blog posts
           </p>
