3d3a279e6fc9b927ea273330e1abf59c8e6c9d3f
Noble Okafor <okafornoble202@gmail.com>
2025-03-22 19:21:55 +0100

    Add project images and custom styles for 3D stack cards

--- src/app/(components)/Projects.tsx
+++ src/app/(components)/Projects.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Project1 from '/public/assets/projects/Project1.jpeg';
+import Project2 from '/public/assets/projects/retroUI.jpeg';
+import '../custom.css';
+
 
 export default function Projects() {
   return (
     <Link href={'/Projects'}>
-    <div className='border-2 flex-1'>Projects</div>
+      <div className='card-container'>
+        <Image
+          src={Project1}
+          alt='openzepelin'
+          className="image-view"
+        />
+        <Image src={Project2} alt='openzepelin' className='image-view' />
+      </div>
     </Link>
-    
-  )
+  );
 }
