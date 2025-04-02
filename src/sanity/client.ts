e9a752b5e18cf5dc2f2ff4a20384e116237c32de
Noble Okafor <okafornoble202@gmail.com>
2025-04-02 22:55:43 +0100

    Sanity live goes LIVE

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from 'next-sanity';
 
 export const client = createClient({
   projectId: 'ajvyyuzz',
-  dataset: '{{PROJECT_DATASET_0}}',
+  dataset: 'production',
   apiVersion: '2024-01-01',
   useCdn: false,
 });
