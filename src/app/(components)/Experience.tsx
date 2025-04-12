8a8924ee2bb359d224d085645b202f79d29b124b
Noble Okafor <okafornoble202@gmail.com>
2025-04-12 21:02:10 +0100

    feat: refactor Experience component to utilize image URL builder for dynamic image rendering

--- src/app/(components)/Experience.tsx
+++ src/app/(components)/Experience.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Image from 'next/image';
 import { defineQuery } from 'next-sanity';
 import { sanityFetch } from '@/sanity/live';
+import imageUrlBuilder from '@sanity/image-url';
+import { client } from '@/sanity/client'; //
 
 import Link from 'next/link';
 
 const GROQ_QUERY = defineQuery(
   `*[_type == "experience" && defined(slug.current)]{_id, title, slug, image}`
 );
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
 
 async function Experience() {
   console.log('👋 Experience component mounted');
@@ -25,18 +33,16 @@ async function Experience() {
         <div className='absolute bottom-0 left-0 right-2 h-5 bg-gradient-to-t from-[#F5F4F2] to-transparent pointer-events-none z-10 '></div>
         <div className='h-full px-2 overflow-y-auto scrollbar-thin scrollbar-thumb-[#727270] scrollbar-track-transparent scrollbar-thumb-rounded-full'>
           {/* <div className='flex items-center pb-2'> */}
-          {events.map((event) => (
-            <div key={event._id}>
+          {events.map(({ _id, title, image }) => (
+            <div key={_id}>
               <Image
-                src={event.image}
-                alt={event.title}
+                src={urlFor(image).width(100).height(100).url()}
+                alt={title}
                 width={30}
                 height={30}
                 className='rounded-full border border-[#DBDAD6]'
               />
-              <p className='font-medium text-xs text-[#464644]'>
-                {event.title}
-              </p>
+              <p className='font-medium text-xs text-[#464644]'>{title}</p>
               <p className='text-xxs'>Sep 2022 - Dec 2022</p>
             </div>
           ))}
