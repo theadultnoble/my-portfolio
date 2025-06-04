5b2f9bedbdbc46252038a82972eac1c7654faa53
theadultnoble <okafornoble202@gmail.com>
2025-06-04 19:21:44 +0100

    chore: update package-lock.json and refactor BlogPosts, ClientSideBlog, and ClientExperience components for consistent styling and improved readability

--- src/app/(components)/blogComp/ClientSideBlog.tsx
+++ src/app/(components)/blogComp/ClientSideBlog.tsx
@@ -1,9 +1,9 @@
-'use client';
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { client } from '@/sanity/client'; //
-import imageUrlBuilder from '@sanity/image-url';
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { client } from "@/sanity/client"; //
+import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(client);
 
@@ -12,41 +12,42 @@ function urlFor(source: any) {
 }
 
 export default function ClientSideBlog({ blogPosts }: { blogPosts: any[] }) {
+  console.log(blogPosts);
   const truncateText = (text: string, maxLength: number) => {
     if (text.length <= maxLength) return text;
-    return text.substring(0, text.lastIndexOf(' ', maxLength)) + '...';
+    return text.substring(0, text.lastIndexOf(" ", maxLength)) + "...";
   };
 
   return (
-    <div className='relative overflow-hidden'>
+    <div className="relative overflow-hidden">
       <div
-        className='flex flex-col divide-y divide-[#DBDAD6]'
+        className="flex flex-col divide-y divide-[#DBDAD6]"
         onScroll={(e) => {
-          const fadeEffect = document.getElementById('fadeEffect');
+          const fadeEffect = document.getElementById("fadeEffect");
           if (fadeEffect) {
             fadeEffect.style.opacity =
-              e.currentTarget.scrollTop > 0 ? '1' : '0';
+              e.currentTarget.scrollTop > 0 ? "1" : "0";
           }
         }}
       >
         {blogPosts.map(({ _id, title, image, URL, body }) => {
           return (
-            <div key={_id} className='flex gap-2 py-2'>
-              <Link href={URL} target='_blank'>
+            <div key={_id} className="flex gap-2 py-2">
+              <Link href={URL} target="_blank">
                 <Image
                   src={urlFor(image).width(300).height(300).url()}
                   alt={title}
                   width={90} // Fixed width
                   height={70} // Fixed height
                   unoptimized
-                  className='rounded-md flex-shrink-0 object-cover'
+                  className="rounded-md flex-shrink-0 object-cover"
                 />
               </Link>
-              <div className='flex flex-col'>
-                <p className='text-[#464644] font-normal pb-1 text-xs'>
+              <div className="flex flex-col">
+                <p className="text-[#464644] font-normal pb-1 text-xs">
                   {truncateText(title, 50)}
                 </p>
-                <p className='text-[#727270] text-xf'>
+                <p className="text-[#727270] text-xf">
                   {truncateText(body, 70)}
                 </p>
               </div>
--- src/app/pages/OpenSource/page.tsx
+++ src/app/pages/OpenSource/page.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React from "react";
+import ClientOpenSource from "./ClientOpenSource";
+import { sanityFetch } from "@/sanity/live";
+import { OPEN_SOURCE_QUERY } from "@/sanity/queries";
 
-export default function page() {
-  return <div>page</div>;
+async function page() {
+  const { data: openSource } = await sanityFetch({
+    query: OPEN_SOURCE_QUERY,
+  });
+  if (!openSource) {
+    return <div>Loading...</div>;
+  }
+  return <ClientOpenSource openSource={openSource} />;
 }
+
+export default page;
