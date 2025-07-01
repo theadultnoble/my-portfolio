9993dc388cd80ca133952080ca61dacabee27f42
theadultnoble <okafornoble202@gmail.com>
2025-07-01 19:05:53 +0100

    chore: add environment configuration and integrate Upstash Redis for token caching

--- src/app/api/playdata/route.js
+++ src/app/api/playdata/route.js
@@ -1,27 +1,30 @@
-import { NextResponse } from 'next/server';
-import axios from 'axios';
+import { NextResponse } from "next/server";
+import axios from "axios";
 
-const SPOTIFY_PLAYLIST_ID = '76Xa7jErorQFcZkrCJZDzz'; // Replace with your actual playlist ID
+const SPOTIFY_PLAYLIST_ID = "76Xa7jErorQFcZkrCJZDzz";
 const SPOTIFY_API_URL = `https://api.spotify.com/v1/playlists/${SPOTIFY_PLAYLIST_ID}/tracks`;
 
 export async function GET(request) {
-  const tokenUrl = new URL('/api/token', request.url).toString();
-  const tokenResponse = await fetch(tokenUrl); 
+  const tokenUrl = new URL("/api/token", request.url).toString();
+  const tokenResponse = await fetch(tokenUrl);
   const { access_token } = await tokenResponse.json();
 
   try {
     const response = await axios.get(SPOTIFY_API_URL, {
-      method: 'GET',
+      method: "GET",
       headers: {
         Authorization: `Bearer ${access_token}`,
       },
     });
 
-    const tracks = response.data
+    const tracks = response.data;
 
     return NextResponse.json(tracks);
   } catch (error) {
-    console.error('Error fetching playlist tracks:', error);
-    return NextResponse.json({ error: 'Failed to fetch playlist tracks' }, { status: 500 });
+    console.error("Error fetching playlist tracks:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch playlist tracks" },
+      { status: 500 }
+    );
   }
 }
--- src/app/api/token/route.js
+++ src/app/api/token/route.js
@@ -1,29 +1,32 @@
-import axios from 'axios';
-import { NextResponse } from 'next/server';
-let cachedToken = null;
-let tokenExpiry = 0;
+import { redis } from "@/app/utilities/redis";
+import axios from "axios";
+import { NextResponse } from "next/server";
+// import { redis } from "./redis";
+
+const TOKEN_KEY = "spotify_access_token";
 
 export async function GET() {
-  const now = Date.now();
-  if (cachedToken && now < tokenExpiry) {
+  let cachedToken = await redis.get(TOKEN_KEY);
+
+  if (cachedToken) {
     return NextResponse.json({ access_token: cachedToken });
   } else {
     const clientID = process.env.SPOTIFY_CLIENT_ID;
     const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
     var tokenOptions = {
-      method: 'POST',
-      url: 'https://accounts.spotify.com/api/token',
+      method: "POST",
+      url: "https://accounts.spotify.com/api/token",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data: `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`,
     };
     const response = await axios(tokenOptions);
-    const data = response.data;
-    cachedToken = data.access_token;
-    tokenExpiry = now + data.expires_in * 1000;
-
+    const cachedToken = response.data.access_token;
+    console.log(cachedToken);
+    const expiresIn = response.data.expires_in;
+    await redis.set(TOKEN_KEY, cachedToken, { ex: expiresIn - 60 });
     return NextResponse.json({ access_token: cachedToken });
   }
 }
